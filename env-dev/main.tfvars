0a7c54c0b33727e1b896de139d3deeec28b3bc84
Sreeni.B <sreenu.reddy002@gmail.com>
2023-07-17 21:14:59 +0530

    Parameter store with KMS

---
+++ env-dev/main.tfvars
@@ -0,0 +1,4 @@
+parameters = [
+  { name = "dev.frontend.catalogue_url", value = "http://catalogue-dev.sreenivasulareddydevops.online:8080/" },
+  { name = "dev.frontend.cart_url", value = "http://cart-dev.sreenivasulareddydevops.online:8080/" },
+]
--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
-resource "aws_ssm_parameter" "parameter" {
-  name = "test.test1"
-  type = "SecureString"
+resource "aws_ssm_parameter" "parameters" {
+  count = length(var.parameters)
+  name = var.parameters[count.index].name
+  type = var.parameters[count.index].value
   value = "Hello World"
   key_id = "97f14d48-2686-4713-9aa1-776591b60d52"
 }
