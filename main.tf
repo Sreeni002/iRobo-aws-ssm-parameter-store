57fbf48a178e54b5f7a24014a808172a1fd7c917
Sreeni.B <sreenu.reddy002@gmail.com>
2023-07-17 20:57:52 +0530

    Create KMS

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name = "test.test1"
-  type = "String"
+  type = "SecureString"
   value = "Hello World"
+  key_id = "97f14d48-2686-4713-9aa1-776591b60d52"
 }
