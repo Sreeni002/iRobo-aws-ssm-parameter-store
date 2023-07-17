07a0325af06145990adc5ec7723025fdb7b145c1
Sreeni.B <sreenu.reddy002@gmail.com>
2023-07-17 22:12:11 +0530

    Accessing list values for parameters

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_ssm_parameter" "parameters" {
   count = length(var.parameters)
   name = var.parameters[count.index].name
-  type = var.parameters[count.index].value
-  value = "Hello World"
+  value = var.parameters[count.index].value
+  type = "String"
   key_id = "97f14d48-2686-4713-9aa1-776591b60d52"
 }
